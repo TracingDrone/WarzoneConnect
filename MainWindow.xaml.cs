67fd8bf1e676bf96a3575ae927d097fb3c861334
Tokino Lynx <tracingdrone@qq.com>
2020-08-19 17:09:10 +0800

    fixed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -37,6 +38,8 @@ namespace WarzoneConnect
                 var fileStream = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "stream.mp4", FileMode.OpenOrCreate);
                 fileStream.Write(tempByte ?? throw new InvalidOperationException(), 0, tempByte.Length);
                 Warzone_Intro.Source = new Uri(AppDomain.CurrentDomain.BaseDirectory + "stream.mp4");
+
+                Warzone_Title.Source = Imaging.CreateBitmapSourceFromHBitmap(IntroMedia.warzone_connect.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
 
                 ShowDialog();
                 Close();
